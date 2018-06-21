cd189f308c26f8273344912285076c34363844ac
Spoondogg <ryan@spoonmedia.ca>
2018-06-21 15:15:49 -0400

    Fixed error with JUMBOTRON case sensitive bgimage creating a new FORMPOST

---
+++ ICARUS/Controllers/MenuListController.cs
@@ -0,0 +1,44 @@
+﻿using ICARUS.Models.Icarus;
+using ICARUS.Models.Icarus.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICARUS.Controllers {
+    
+    public class MenuListController : ContainerController {
+
+        public MenuListController() : base("MenuList") {
+
+        }
+
+        /// <summary>
+        /// Instantiate a Container using List defaults
+        /// </summary>
+        /// <returns></returns>
+        public override Container make(FormPost formPost = null) {
+            MenuList obj = (formPost == null)
+                ? new MenuList()
+                : new MenuList(formPost);
+
+            obj.setAuthorId(User.Identity.Name);
+            return obj;
+        }
+
+        /// <summary>
+        /// Select a single Main element
+        /// </summary>
+        /// <param name="ctx"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public override Container select(ObjectDBContext ctx, int id) {
+            MenuList model = (MenuList)ctx.MenuLists.Single(m =>
+                   m.id == id && (m.authorId == User.Identity.Name || m.shared == 1)
+                );
+            return model;
+        }
+    }
+}
---
+++ ICARUS/Models/Icarus/Elements/MenuList.cs
@@ -0,0 +1,32 @@
+﻿using ICARUS.Models.Icarus.Elements.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ICARUS.Models.Icarus.Elements {
+
+    /// <summary>
+    /// A generic List element object
+    /// </summary>
+    public class MenuList : Container {
+
+        /// <summary>
+        /// A Collection of List Items
+        /// </summary>
+        /// <param name="attributes"></param>
+        public MenuList() : base("DIV", new MODEL() {
+            label = "MENULIST"
+        }) {
+            this.className = "MENULIST";
+        }
+
+        /// <summary>
+        /// Constructs a LIST with the given id and label attributes already set
+        /// </summary>
+        /// <param name="formPost"></param>
+        public MenuList(FormPost formPost) : base("DIV", formPost) {
+
+        }
+    }
+}
