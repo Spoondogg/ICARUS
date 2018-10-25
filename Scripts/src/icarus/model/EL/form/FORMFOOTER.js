5784c653589a14c1edef6259a025adc7834048ce
Spoondogg <ryan@spoonmedia.ca>
2018-10-25 15:46:22 -0400

    Now uses addClass() instead of directly modifying the el

--- Scripts/src/icarus/model/EL/form/FORMFOOTER.js
+++ Scripts/src/icarus/model/EL/form/FORMFOOTER.js
@@ -2,21 +2,18 @@
 import { ALIGN } from '../../../enums/ALIGN.js';
 import BUTTONGROUP from '../group/buttongroup/BUTTONGROUP.js';
 import FOOTER from '../footer/FOOTER.js';
-//import { SIZE } from '../../../enums/SIZE.js';
-/**
-    A generic footer that should be placed at the bottom of content
+/** A generic footer that should be placed at the bottom of content
     @class
     @extends FOOTER
 */
 export default class FORMFOOTER extends FOOTER {
-	/**
-	    Constructs a Form Footer
+	/** Constructs a Form Footer
 	    @param {EL} node The object to contain the table
 	    @param {MODEL} model Object model
 	*/
 	constructor(node, model) {
 		super(node, model);
-		this.el.className = 'btn-group-justified form-footer';
+		this.addClass('btn-group-justified form-footer');
         this.buttonGroup = new BUTTONGROUP(this, null, ALIGN.VERTICAL); // Left aligned button group SIZE.MED
 	}
 }
