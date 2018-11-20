c21ac31728b297cb045e72df07d4058b1cca841d
Spoondogg <ryan@spoonmedia.ca>
2018-11-20 15:54:58 -0500

    LOADER and CONSOLE appear as a DIALOG that slides up from the bottom

--- Scripts/src/icarus/model/EL/dialog/loader/LOADER.js
+++ Scripts/src/icarus/model/EL/dialog/loader/LOADER.js
@@ -1,5 +1,5 @@
 /** @module */
-import DIALOG, { ATTRIBUTES, DIV, MODEL } from '../../dialog/DIALOG.js';
+import DIALOG, { DIV, MODEL } from '../../dialog/DIALOG.js'; //ATTRIBUTES
 import CONSOLE from '../../ul/console/CONSOLE.js';
 import PROGRESSBAR from './PROGRESSBAR.js';
 /** A Dialog showing loading details
@@ -10,58 +10,52 @@ export default class LOADER extends DIALOG {
 	/** Constructs a LOADER dialog
         @param {number} value Percentage complete (integer)
         @param {string} label The header text for this modal	    
-        @param {string} text Text that appears in modal's well
+        param {string} text Text that appears in modal's well
         @param {CONTAINER} container Parent Container
     */
-	constructor(value = 0, label = '', text = '', container = null) {
+	constructor(value = 0, label = '', container = null) {
 		super(new MODEL().set({
 			value,
 			label,
-            text,
             container
 		}));
 		this.addClass('loader');
-		//this.el.setAttribute('name', 'LOADER');
 		this.progress = new DIV(this.body, new MODEL('progress'));
 		this.progressBar = new PROGRESSBAR(this.progress, new MODEL());
-		this.console = new CONSOLE(this.body, new MODEL(new ATTRIBUTES({
-			'class': 'console collapse in',
-			'aria-expanded': false
-		})));
+		this.console = new CONSOLE(this.body, new MODEL('console collapse in'));
 		this.progressBar.el.onclick = () => {
 			$(this.console.el).collapse('toggle');
 		};
-		if (this.well) {
-			$(this.progress.el).insertBefore(this.well.el);
-			$(this.console.el).insertBefore(this.well.el);
-		}
 		this.log(value);
 	}
 	/** Sets the progress bar status
 		@param {number} value Percentage as integer (ie: 50 means 50%).
 		@param {string} text Text displayed inside progress bar.  
 	    @param {boolean} show If true, the log will be displayed
-	    @returns {void}
+	    @returns {Promise} Promise to return this LOADER after success
 	*/
 	log(value, text, show = false) {
-        try {
-            this.progressBar.el.style.width = value + '%';
-            this.progressBar.el.setAttribute('aria-valuenow', value);
-            if (text) {
-                let txt = text.substr(0, 32) + '...';
-                this.progressBar.setInnerHTML(txt);
-                this.console.addEntry(text);
-                console.log(text);
-            }
-            if (show) {
-                this.show();
-                if (value === 100) {
-                    this.close();
+        return new Promise((resolve, reject) => {
+            try {
+                this.progressBar.el.style.width = value + '%';
+                this.progressBar.el.setAttribute('aria-valuenow', value);
+                if (text) {
+                    let txt = text.substr(0, 32) + '...';
+                    this.progressBar.setInnerHTML(txt);
+                    this.console.addEntry(text);
+                    console.log(text);
                 }
+                if (show) {
+                    this.show();
+                    if (value === 100) {
+                        this.close();
+                    }
+                }
+                resolve();
+            } catch (e) {
+                reject(e);
             }
-        } catch (e) {
-			console.log('Unable to find loader');
-		}
+        });
 	}
 	/** Shows the Loader Console
         @returns {void}
