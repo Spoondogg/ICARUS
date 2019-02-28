53850e59c4f3cfa24cb609391afb2b383bbc86c3
Spoondogg <ryan@spoonmedia.ca>
2019-02-28 12:22:56 -0500

    Removing empty spaces

--- Scripts/src/icarus/model/EL/container/PANE.js
+++ Scripts/src/icarus/model/EL/container/PANE.js
@@ -14,30 +14,29 @@ export default class PANE extends DIV {
 	constructor(node, model) {
 		super(node, model);
 		this.addClass('pane');
-        this.implement(new Swipeable(this, 150));
-        this.swipeUp = () => {
-            this.getMain().focusBody();
-            //console.log('collapsing navheader');
-            this.getContainer().navheader.collapse();   
-        };
-        this.swipeDown = () => {
-            this.getMain().focusBody();
-            //console.log('expanding navheader');
-            this.getContainer().navheader.expand();            
-        };
-        this.swipeLeft = () => {
-            let [navMenu] = this.getMain().navheader.tabs.get('sidebar-user', 'NAVITEMICON');
-            navMenu.el.dispatchEvent(new Activate(navMenu));
-            //console.log('expanding navheader');
-            //this.getContainer().navheader.expand();
-        };
-        this.swipeRight = () => {
-            let [navMenu] = this.getMain().navheader.tabs.get('document-map', 'NAVITEMICON');
-            navMenu.el.dispatchEvent(new Activate(navMenu));
-            //console.log('expanding navheader');
-            //this.getContainer().navheader.expand();
-        };
-    }
-    
+		this.implement(new Swipeable(this, 150));
+		this.swipeUp = () => {
+			this.getMain().focusBody();
+			//console.log('collapsing navheader');
+			this.getContainer().navheader.collapse();
+		};
+		this.swipeDown = () => {
+			this.getMain().focusBody();
+			//console.log('expanding navheader');
+			this.getContainer().navheader.expand();
+		};
+		this.swipeLeft = () => {
+			let [navMenu] = this.getMain().navheader.tabs.get('sidebar-user', 'NAVITEMICON');
+			navMenu.el.dispatchEvent(new Activate(navMenu));
+			//console.log('expanding navheader');
+			//this.getContainer().navheader.expand();
+		};
+		this.swipeRight = () => {
+			let [navMenu] = this.getMain().navheader.tabs.get('document-map', 'NAVITEMICON');
+			navMenu.el.dispatchEvent(new Activate(navMenu));
+			//console.log('expanding navheader');
+			//this.getContainer().navheader.expand();
+		};
+	}
 }
 export { CONTAINER, EL, MODEL }
