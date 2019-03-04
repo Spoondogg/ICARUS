718733129f1913131325fbca6f21f28705366dd8
Spoondogg <ryan@spoonmedia.ca>
2019-03-04 00:06:04 -0500

    Article now shows author and date created

--- Scripts/src/icarus/model/EL/article/ARTICLE.js
+++ Scripts/src/icarus/model/EL/article/ARTICLE.js
@@ -1,5 +1,6 @@
 /** @module */
 import CONTAINER, { Expand, MODEL } from '../container/CONTAINER.js'; //, { ATTRIBUTES, EL, MODEL }
+import SPAN from '../span/SPAN.js';
 /** A generic ARTICLE Element
     @class
     @extends CONTAINER
@@ -16,6 +17,9 @@ export default class ARTICLE extends CONTAINER {
     constructElements() {
         if (this.dataId > 0) {
             this.createEditableElement('header', this.body.pane);
+            let date = this.getDateCreated();
+            this.articleDate = new SPAN(this.body.pane, new MODEL('date-created').set('innerHTML', date.date));
+            this.articleAuthor = new SPAN(this.body.pane, new MODEL('author').set('innerHTML', this.authorId));
         } else {
             console.log('No data exists for ' + this.className);
             this.navheader.el.dispatchEvent(new Expand(this));
