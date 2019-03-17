84ca1b887211052f8cbf882ddc814a6b1fc19ac1
Spoondogg <ryan@spoonmedia.ca>
2019-03-17 14:32:02 -0400

    Remove commented code

--- Scripts/src/icarus/model/EL/nav/navbar/navfooter/NAVFOOTER.js
+++ Scripts/src/icarus/model/EL/nav/navbar/navfooter/NAVFOOTER.js
@@ -1,5 +1,5 @@
 /** @module */
-import NAVBAR, { ANCHOR, Collapse, Collapsible, EL, Expand, ICONS, LIST, MENU, MODEL, NAVITEM, NAVITEMICON } from '../NAVBAR.js';
+import NAVBAR, { ANCHOR, Collapse, Collapsible, EL, Expand, LIST, MENU, MODEL, NAVITEM, NAVITEMICON } from '../NAVBAR.js';
 /** A NAVBAR that acts as a CONTAINER Header
     @class
     @extends NAVBAR
@@ -12,16 +12,6 @@ export default class NAVFOOTER extends NAVBAR {
 	constructor(node, model) {
 		super(node, model, true);
 		this.addClass('nav-footer');
-		//$(this.menus.el).insertBefore(this.tabs.el);
-		/*this.tab = this.tabs.addNavItemIcon(new MODEL('tab-console').set({
-		    icon: ICONS.CONSOLE,
-		    label: 'CONSOLE'
-		}));*/
-		//node.el.addEventListener('activate', () => this.tab.el.dispatchEvent(new Activate()));
-		//node.el.addEventListener('deactivate', () => this.tab.el.dispatchEvent(new Deactivate()));
-		//this.tab.el.addEventListener('activate', () => node.body.el.dispatchEvent(new Expand()));
-		//this.tab.el.addEventListener('deactivate', () => node.body.el.dispatchEvent(new Collapse()));
-		// Create initial OPTIONS Tab and Menu
 		this.addOptionsMenu();
 	}
 }
