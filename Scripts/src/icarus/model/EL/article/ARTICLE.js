4d3acce2f1b922a07ac3d44d997d2629a2458892
Spoondogg <ryan@spoonmedia.ca>
2019-04-23 00:16:17 -0400

    Article data elements now inside collapsible element

--- Scripts/src/icarus/model/EL/article/ARTICLE.js
+++ Scripts/src/icarus/model/EL/article/ARTICLE.js
@@ -1,5 +1,6 @@
 /** @module */
-import CONTAINER, { Activate, MODEL } from '../container/CONTAINER.js';
+import CONTAINER, { Activate, COLLAPSIBLE, Expand, MODEL } from '../container/CONTAINER.js';
+//import DIV from '../div/DIV.js';
 import SPAN from '../span/SPAN.js';
 /** A generic ARTICLE Element
     @class
@@ -12,15 +13,17 @@ export default class ARTICLE extends CONTAINER {
 	*/
 	constructor(node, model) {
 		super(node, 'ARTICLE', model, ['JUMBOTRON', 'FORM', 'SECTION']);
-		this.addClass('article');
+        this.addClass('article');
+        this.articleHeader = new COLLAPSIBLE(this.body.pane, new MODEL('header'));
 	}
 	constructElements() {
 		return this.chain(() => {
-			if (this.dataId > 0) {
+            if (this.dataId > 0) {                
 				this.createEditableElement('header', this.body.pane);
 				let date = this.getDateCreated();
-				this.articleDate = new SPAN(this.body.pane, new MODEL('date-created').set('innerHTML', date.date));
-				this.articleAuthor = new SPAN(this.body.pane, new MODEL('author').set('innerHTML', this.authorId));
+                this.articleDate = new SPAN(this.articleHeader.pane, new MODEL('date-created').set('innerHTML', date.date));
+                this.articleAuthor = new SPAN(this.articleHeader.pane, new MODEL('author').set('innerHTML', this.authorId));
+                this.articleHeader.el.dispatchEvent(new Expand(this.articleHeader));
 			}
 			this.navheader.tab.el.dispatchEvent(new Activate(this.navheader.tab));
 		});
