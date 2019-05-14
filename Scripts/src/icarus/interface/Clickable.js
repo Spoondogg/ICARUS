cf01dd934d3d33190061816b352961aa7df22ea5
Spoondogg <ryan@spoonmedia.ca>
2019-05-14 15:13:07 -0400

    Implement LongClick Events

--- Scripts/src/icarus/interface/Clickable.js
+++ Scripts/src/icarus/interface/Clickable.js
@@ -3,6 +3,7 @@ import IFACE, { EL, MODEL } from './IFACE.js';
 import Activate from '../event/Activate.js';
 import Deactivate from '../event/Deactivate.js';
 import Deselect from '../event/Deselect.js';
+import LongClick from '../event/LongClick.js';
 import Select from '../event/Select.js';
 import Selectable from './Selectable.js';
 import Switchable from './Switchable.js';
@@ -15,8 +16,8 @@ export default class Clickable extends IFACE {
         A 'clickable' element is capable of performing 5 separate events
             - Single Click toggles Activation/Deactivation
             - Double Click toggles Selection/Deselection
-            - Long Click toggles the CONTEXT Event
-        @todo Stop Propogation could use some work
+            - Long Click performs contextual Events, Triggers etc
+        @todo Stop Propagation could use some work
         @param {EL} node Class to implement this interface (Typically 'this')
         @param {MODEL} options Click Timer Options
 	*/
@@ -26,13 +27,43 @@ export default class Clickable extends IFACE {
 		stopPropagation: true
 	})) {
 		super(node, 'clickable');
-		this.options = options;
+        this.options = options;
 		node.implement(new Switchable(node));
 		node.implement(new Selectable(node));
 		node.el.style.webkitTouchCallout = 'none';
 		node.timer = null;
-		node.touchtime = 0; // mobile double click detection           
-	}
+        node.touchtime = 0; // mobile double click detection
+    }
+    /** Disables the browser context menu
+        @description This is generally not considered a good practice.  Use sparingly and be sure to enable again once you're done
+        @see https://stackoverflow.com/questions/737022/how-do-i-disable-right-click-on-my-web-page
+        @returns {Promise<boolean>} Resolves true on completion
+    */
+    disableContextMenu() {
+        return new Promise((resolve) => {
+            document.addEventListener('contextmenu', this.preventDefault);
+            resolve(true);
+        });       
+    }
+    /** Enables the Context Menu in cases where it may have been disabled
+        @param {number} delay Delay
+        @returns {Promise<boolean>} Resolves true on completion
+    */
+    enableContextmenu(delay = 500) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                document.removeEventListener('contextmenu', this.preventDefault);
+            }, delay);
+            resolve(true);
+        });
+    }
+    /** Calls Event.preventDefault attached to an Event Listener
+        @param {Event} ev Event
+        @returns {void}
+    */
+    preventDefault(ev) {
+        ev.preventDefault();
+    }
 	/** Adds listeners where applicable
 	    @param {EL} node Element to append listeners
 	    @returns {void}
@@ -45,7 +76,7 @@ export default class Clickable extends IFACE {
 		node.el.addEventListener('mouseup', (ev) => node.pressUp(ev));
 		node.el.addEventListener('touchstart', (ev) => node.pressDown(ev), { passive: true });
 		node.el.addEventListener('touchend', (ev) => node.pressUp(ev), { passive: true });
-		node.el.addEventListener('click', (ev) => node.pressed(ev));
+        node.el.addEventListener('click', (ev) => node.pressed(ev));
 	}
 	startTimer(node) {
 		clearTimeout(node.timer);
@@ -54,12 +85,42 @@ export default class Clickable extends IFACE {
 	resetTimer(node) {
 		node.touchtime = 0;
 		clearTimeout(node.timer);
-	}
+    }
+    /** On PressDown Event, starts a timer that triggers the Long Press Event
+        @param {EL} node Element to implement methods
+        @param {Event} ev Event
+		@returns {Promise<any>} Promise to resolve given function
+	*/
+    pressDown(node, ev) {
+        node.addClass('press').then(() => {
+            //console.log(node.toString() + '.press-down()');
+            node.timer = window.setTimeout(() => {
+                this.disableContextMenu().then(() => {
+                    ev.stopPropagation();
+                    this.startTimer(node);
+                    Promise.resolve(node.longclick()).then(
+                        () => node.removeClass('press').then(
+                            () => this.enableContextmenu()));
+                });
+            }, this.options.longClickDelay);
+        });
+    }
+    /** On PressUp Event, cancels PressDown Timer
+        @param {EL} node Element to implement methods
+        @param {Event} ev Event
+		@returns {void} 
+	*/
+    pressUp(node, ev) {
+        //console.log(node.toString() + '.press-up()');
+        ev.stopPropagation();
+        node.removeClass('press');
+        clearTimeout(node.timer);
+    }
 	/** Appends Interface methods to class that implements them
 	    @param {EL} node Element to implement methods
 	    @returns {void}
 	*/
-	setMethods(node) {
+    setMethods(node) {
 		/** Toggle the 'active' state of this Element and dispatch appropriate Event
 		    @returns {void}
 		*/
@@ -68,29 +129,20 @@ export default class Clickable extends IFACE {
 		    @returns {void}
 		*/
 		this.methods.dblclick = () => this.toggle('selected', new Select(node), new Deselect(node));
-		/** No event is called for a long-click at this time
+		/** Dispatches a LongClick Event
 		    @returns {void}
 		*/
-		this.methods.longclick = () => null; //console.log('longclick-down', node);
+        this.methods.longclick = () => node.el.dispatchEvent(new LongClick(node));
 		/** On PressDown Event, starts a timer that triggers the Long Press Event
             @param {Event} ev Event
 		    @returns {Promise<any>} Promise to resolve given function
 		*/
-		this.methods.pressDown = (ev) => {
-			node.timer = window.setTimeout(() => {
-				ev.stopPropagation();
-				this.startTimer(node);
-				Promise.resolve(node.longclick());
-			}, this.options.longClickDelay);
-		}
+        this.methods.pressDown = (ev) => this.pressDown(node, ev);
 		/** On PressUp Event, cancels PressDown Timer
             @param {Event} ev Event
 		    @returns {void} 
 		*/
-		this.methods.pressUp = (ev) => {
-			ev.stopPropagation();
-			clearTimeout(node.timer);
-		}
+        this.methods.pressUp = (ev) => this.pressUp(node, ev);
 		/** Press Event occurs after Press Down/Up events complete, 
 		    signalling that the element has been pressed
             @param {Event} ev Event
@@ -98,31 +150,38 @@ export default class Clickable extends IFACE {
 		*/
 		this.methods.pressed = (ev) => {
 			//console.log('Pressed', this.timer, ev);
-			ev.stopPropagation();
-			try {
-				if (node.touchtime === 0) {
-					node.touchtime = new Date().getTime();
-					setTimeout(() => {
-						if (node.touchtime !== 0) {
-							//console.log('click-single', node.timer, node);
-							this.resetTimer(node);
-							Promise.resolve(node.click());
-						}
-					}, this.options.delay);
-				} else if (new Date().getTime() - node.touchtime < this.options.delay) {
-					//console.log('click-double', node.timer, node);
-					this.resetTimer(node);
-					Promise.resolve(node.dblclick());
-				} else {
-					//console.log('longclick-up', node.timer, node);
-					this.resetTimer(node);
-				}
+            ev.stopPropagation();
+            try {
+                this.disableContextMenu().then(() => {
+                    if (node.touchtime === 0) {
+                        node.touchtime = new Date().getTime();
+                        setTimeout(() => {
+                            if (node.touchtime !== 0) {
+                                //console.log('click-single', node.timer, node);
+                                node.removeClass('press');
+                                this.resetTimer(node);
+                                Promise.resolve(node.click()).then(() => this.enableContextmenu());
+                            }
+                        }, this.options.delay);
+                    } else if (new Date().getTime() - node.touchtime < this.options.delay) {
+                        //console.log('click-double', node.timer, node);
+                        node.removeClass('press');
+                        this.resetTimer(node);
+                        Promise.resolve(node.dblclick()).then(() => this.enableContextmenu());
+                    } else {
+                        //console.log('longclick-up', node.timer, node);
+                        node.removeClass('press');
+                        this.resetTimer(node);
+                        Promise.resolve(true).then(() => this.enableContextmenu());
+                    }
+                });                
 			} catch (e) {
-				console.warn('error', e);
+                console.warn('Clickable Error', e);
+                node.removeClass('press');
 				this.resetTimer(node);
-				Promise.reject(e);
-			}
+                Promise.reject(e).then(() => this.enableContextmenu());
+            }
 		}
 	}
 }
-export { Activate, Deactivate, Deselect, EL, Select, Selectable, Switchable }
+export { Activate, Deactivate, Deselect, EL, IFACE, Select, Selectable, Switchable }
