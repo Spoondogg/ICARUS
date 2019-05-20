bf0e09b2aa079c27da943773972f4074923970a3
Spoondogg <ryan@spoonmedia.ca>
2019-05-20 01:01:27 -0400

    Simple implementation of 'Add to Home Screen' logic

--- Scripts/src/icarus/helper/WATERMARK.js
+++ Scripts/src/icarus/helper/WATERMARK.js
@@ -15,6 +15,19 @@ export default class WATERMARK {
 		console.log("|__ --|  _  |  _  |  _  |     |        |  -__|  _  |  ||  _  |__|  __|  _  |");
 		console.log("|_____|   __|_____|_____|__|__|__|__|__|_____|_____|__||___._|__|____|___._|");
 		console.log("      |__|                                                                  ");
-		console.log("\n\n");
-	}
+        console.log("\n\n");
+        this.needsToSeePrompt();
+    }
+    /** Checks if environment should launch Add to Home Screen prompt 
+        @see https://dockyard.com/blog/2017/09/27/encouraging-pwa-installation-on-ios
+        @returns {boolean} true if apple
+    */
+    needsToSeePrompt() {
+        if (navigator.standalone) {
+            return false;
+        }
+        let isApple = ['iPhone', 'iPad', 'iPod'].includes(navigator.platform);
+        console.log('isApple', isApple);
+        return isApple;
+    }
 }
