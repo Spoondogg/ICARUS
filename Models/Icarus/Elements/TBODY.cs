28d317b76388a98baa9a0e4b8f246eca339f21de
Spoondogg <ryan@spoonmedia.ca>
2019-05-21 15:24:07 -0400

    Created TABLE related element classes and controllers

---
+++ Models/Icarus/Elements/TBODY.cs
@@ -0,0 +1,33 @@
+﻿using ICARUS.Models.Icarus.Elements.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ICARUS.Models.Icarus.Elements {
+
+    /// <summary>
+    /// Represents a table header
+    /// </summary>
+    public class TBODY : Container {
+
+        /// <summary>
+        /// TBODY
+        /// </summary>
+        /// <param name="attributes"></param>
+        public TBODY() : base("TBODY", new MODEL() {
+            label = "TBODY"
+        }) {
+
+        }
+
+        /// <summary>
+        /// Construct a TBODY based on the provided Form Post
+        /// </summary>
+        /// <param name="formPost"></param>
+        public TBODY(FormPost formPost) : base("TBODY", formPost) {
+
+        }
+    }
+}
---
+++ Models/Icarus/Elements/TR.cs
@@ -0,0 +1,33 @@
+﻿using ICARUS.Models.Icarus.Elements.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ICARUS.Models.Icarus.Elements {
+
+    /// <summary>
+    /// Represents a table row
+    /// </summary>
+    public class TR : Container {
+
+        /// <summary>
+        /// Table Row
+        /// </summary>
+        /// <param name="attributes"></param>
+        public TR() : base("TR", new MODEL() {
+            label = "TR"
+        }) {
+
+        }
+
+        /// <summary>
+        /// Construct a THEAD based on the provided Form Post
+        /// </summary>
+        /// <param name="formPost"></param>
+        public TR(FormPost formPost) : base("TR", formPost) {
+
+        }
+    }
+}
