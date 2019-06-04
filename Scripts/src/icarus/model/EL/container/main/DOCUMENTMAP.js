539327761de2896c37e7099c986d49dfcaada501
Spoondogg <ryan@spoonmedia.ca>
2019-06-03 22:44:33 -0400

    Created REFERENCE and DOCUMENTMAP classes

---
+++ Scripts/src/icarus/model/EL/container/main/DOCUMENTMAP.js
@@ -0,0 +1,12 @@
+﻿/** @module */
+import REFERENCE from '../REFERENCE.js';
+/** A DOCUMENTMAP acts as a top level REFERENCE that
+    is made up of many nested REFERENCE(s)
+    @class
+*/
+export default class DOCUMENTMAP extends REFERENCE {
+    constructor(node, model) {
+        super(node, model);
+        this.addClass('document-map');
+    }
+}
