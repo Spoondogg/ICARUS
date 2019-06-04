66fba475a4f09a8665b8b8543293d090f150a7eb
Spoondogg <ryan@spoonmedia.ca>
2019-06-03 22:44:33 -0400

    Created REFERENCE and DOCUMENTMAP classes

---
+++ Scripts/src/icarus/model/EL/container/REFERENCE.js
@@ -0,0 +1,25 @@
+﻿/** @module */
+import { ICONS } from '../../../enums/ICONS.js';
+import NAVHEADER from '../nav/navbar/navheader/NAVHEADER.js';
+/** A REFERENCE represents a collection of menus and tabs representing the MODEL
+    of its given CONTAINER as part of the document-map
+    @class
+*/
+export default class REFERENCE extends NAVHEADER {
+    constructor(node, model) {
+        super(node, model);
+        /** The CONTAINER that this REFERENCE is referring to
+            
+        */
+        //this.container = this.required(model.container);
+        this.name = this.required(model.name);
+        this.addClass('reference');
+        this.addTabbableMenu('tab1', 'Tab1', ICONS.FLAG, [
+            {
+                label: 'ONE',
+                icon: ICONS.FLAG,
+                name: 'ONE'
+            }
+        ], false);
+    }
+}
