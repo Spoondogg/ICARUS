e278f93a1b4dbf81b7e43cbde86d1307862ce37b
Spoondogg <ryan@spoonmedia.ca>
2019-06-09 00:44:03 -0400

    TGROUP now extends container as expected

--- Scripts/src/icarus/model/EL/table/TGROUP/TGROUP.js
+++ Scripts/src/icarus/model/EL/table/TGROUP/TGROUP.js
@@ -5,30 +5,36 @@ import TD from './row/column/TD.js';
 import TH from './row/column/TH.js';
 import TR from './row/TR.js';
 /** A TABLE GROUP (TGROUP) element: ie: THEAD, TBODY, TFOOT */
-export default class TGROUP extends EL {
+export default class TGROUP extends CONTAINER {
 	/** Constructs a table group element
         @param {EL} node Parent
         @param {string} element HTML element Tag
         @param {MODEL} model The model
     */
 	constructor(node, element, model) {
-        super(node, element, model);
+        super(node, element, model, ['TR']);
         this.addClass('table-group');
         this.implement(new Clickable(this));
+    }
+    constructElements() {
+        return Promise.resolve();
     }
 	/** Adds the given table-row to this table-group
 	    @param {MODEL} model Object model
 	    @returns {TR} A Table row
 	*/
     addTr(model) {
-        let containerList = this.className === 'TBODY' ? ['TD'] : ['TH'];
-        return this.addChild(new TR(this, model, containerList));
+        return this.addChild(new TR(this.childLocation, model, this.className === 'TBODY' ? ['TD'] : ['TH']));
     }
     /** Returns this group's table
         @returns {TABLE} Table
     */
     getTable() {
         return this.getProtoTypeByClass('TABLE');
+    }
+
+    getRows() {
+        return this.get(null, 'TR');
     }
 }
 export { CONTAINER, EL, Expand, MODEL, TABLE, TD, TH, TR }
