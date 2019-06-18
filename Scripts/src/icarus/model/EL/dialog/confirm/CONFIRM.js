a6d291788631e1f67b1d93280478a6b8362635b2
Spoondogg <ryan@spoonmedia.ca>
2019-06-18 12:44:46 -0400

    Added static CONFIRM.confirmMethodCall

--- Scripts/src/icarus/model/EL/dialog/confirm/CONFIRM.js
+++ Scripts/src/icarus/model/EL/dialog/confirm/CONFIRM.js
@@ -1,22 +1,23 @@
 /** @module */
-import DIALOG, { ATTRIBUTES, DIALOGMODEL, DIV, EL, ICONS, MODEL } from '../DIALOG.js';
+import PROMPT, { ATTRIBUTES, DIALOG, DIALOGMODEL, DIV, EL, ICONS, MODEL } from '../prompt/PROMPT.js';
 //import FORM from '../../form/FORM.js';
 /** A DIALOG with an embedded FORM that can be used to recieve input
     @description Creates a modal and displays a text well and any included buttons
     @class
-    @extends DIALOG
 */
 export default class CONFIRM extends DIALOG {
 	/** Constructs a CONFIRM Dialog
         @param {DIALOGMODEL} model DIALOG MODEL
         @param {Function} onConfirm Function called on confirm
-        @param {Function} onCancel Function called on cancel
+        @param {Function} [onCancel] Function called on cancel
     */
-	constructor(model, onConfirm, onCancel) {
+	constructor(model, onConfirm, onCancel = null) {
         super(model);
         this.addClass('confirm');
         this.onConfirm = onConfirm;
-        this.onCancel = onCancel;
+        if (onCancel !== null) {
+            this.onCancel = onCancel;
+        }
         this.btnConfirm = this.footer.buttonGroup.addButton('Confirm', ICONS.CONFIRM);
         this.btnConfirm.el.addEventListener('click', () => {
             this.onConfirm();
@@ -37,10 +38,27 @@ export default class CONFIRM extends DIALOG {
     onConfirm() {
         console.log('CONFIRMED!');
     }
-
+    /** Function to call when PROMPT is cancelled
+        @returns {void}
+    */
     onCancel() {
         console.log('CANCELLED!');
     }
-
+    /** Confirms before calling method
+        @param {string} label PROMPT Label
+        @param {string} text PROMPT Text
+        @param {Function} onConfirm Function called on confirm
+        @param {Function} onCancel Function called on cancel
+        @returns {void}
+    */
+    static confirmMethodCall(label, text, onConfirm, onCancel) {
+        let confirm = new CONFIRM(new DIALOGMODEL(new MODEL(), {
+            container: null,
+            caller: null,
+            label,
+            text
+        }), onConfirm, onCancel);
+        confirm.showDialog();
+    }
 }
-export { ATTRIBUTES, DIALOG, DIALOGMODEL, DIV, EL, MODEL }
+export { ATTRIBUTES, DIALOG, DIALOGMODEL, DIV, EL, MODEL, PROMPT }
