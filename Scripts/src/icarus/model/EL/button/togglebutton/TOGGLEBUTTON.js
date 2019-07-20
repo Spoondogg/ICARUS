74760aa502ba0b1009b11a1970701cc37ac122a9
Spoondogg <ryan@spoonmedia.ca>
2019-07-19 20:13:39 -0400

    Cleanup and optimization

--- Scripts/src/icarus/model/EL/button/togglebutton/TOGGLEBUTTON.js
+++ Scripts/src/icarus/model/EL/button/togglebutton/TOGGLEBUTTON.js
@@ -1,25 +1,23 @@
 /** @module */
-import BUTTON, { ICONS } from '../BUTTON.js';
-import MENU, { MODEL } from '../../nav/menu/MENU.js';
+import MENU, { Collapse, Expand, MODEL } from '../../nav/menu/MENU.js';
+import SWITCH, { Activate, BUTTON, Deactivate, ICONS } from '../switch/SWITCH.js';
 import STRING from '../../../../STRING.js';
 /** Button that show/hides a list of options
     @class
     @extends BUTTON
 */
-export default class TOGGLEBUTTON extends BUTTON {
+export default class TOGGLEBUTTON extends SWITCH {
 	/** Construct a toggle button
 	    @param {EL} node The parent object
 	    @param {string} label The label
 	    @param {string} glyphicon glyphicon
 	    @param {string} buttonType buttonType
-	 */
+	*/
 	constructor(node, label, glyphicon, buttonType) {
 		super(node, label, glyphicon, buttonType);
-		this.addClass('dropdown-toggle');
-		this.el.setAttribute('data-toggle', 'dropdown');
-		this.el.setAttribute('aria-haspopup', 'true');
-		this.el.setAttribute('aria-expanded', 'false');
-		this.menu = new MENU(node, new MODEL().set('name', new STRING(label).friendly()));
+        this.menu = new MENU(node, new MODEL().set('name', new STRING(label).friendly()));
+        this.el.addEventListener('activate', () => this.menu.el.dispatchEvent(new Expand(this.el)));
+        this.el.addEventListener('deactivate', () => this.menu.el.dispatchEvent(new Collapse(this.el)));
 	}
 }
-export { BUTTON, ICONS, MENU }
+export { Activate, BUTTON, Collapse, Deactivate, Expand, SWITCH, ICONS, MENU }
