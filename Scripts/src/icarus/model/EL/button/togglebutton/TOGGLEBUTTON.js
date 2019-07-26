04991107ceac67bef19249c8e4197241525949d8
Spoondogg <ryan@spoonmedia.ca>
2019-07-26 09:17:12 -0400

    Improved constructor using attributes struct

--- Scripts/src/icarus/model/EL/button/togglebutton/TOGGLEBUTTON.js
+++ Scripts/src/icarus/model/EL/button/togglebutton/TOGGLEBUTTON.js
@@ -1,5 +1,5 @@
 /** @module */
-import MENU, { Collapse, Expand, MODEL } from '../../nav/menu/MENU.js';
+import MENU, { Collapse, EL, Expand, MODEL } from '../../nav/menu/MENU.js';
 import SWITCH, { Activate, BUTTON, Deactivate, ICONS } from '../switch/SWITCH.js';
 import STRING from '../../../../STRING.js';
 /** Button that show/hides a list of options
@@ -9,15 +9,13 @@ import STRING from '../../../../STRING.js';
 export default class TOGGLEBUTTON extends SWITCH {
 	/** Construct a toggle button
 	    @param {EL} node The parent object
-	    @param {string} label The label
-	    @param {string} glyphicon glyphicon
-	    @param {string} buttonType buttonType
+        @param {ButtonAttributes} attributes Button Attributes
 	*/
-	constructor(node, label, glyphicon, buttonType) {
-		super(node, label, glyphicon, buttonType);
-        this.menu = new MENU(node, new MODEL().set('name', new STRING(label).friendly()));
+	constructor(node, attributes) {
+		super(node, attributes);
+        this.menu = new MENU(node, new MODEL().set('name', new STRING(attributes.label).friendly()));
         this.el.addEventListener('activate', () => this.menu.el.dispatchEvent(new Expand(this.el)));
         this.el.addEventListener('deactivate', () => this.menu.el.dispatchEvent(new Collapse(this.el)));
 	}
 }
-export { Activate, BUTTON, Collapse, Deactivate, Expand, SWITCH, ICONS, MENU }
+export { Activate, BUTTON, Collapse, Deactivate, EL, Expand, SWITCH, ICONS, MENU }
