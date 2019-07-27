251ab3e2537af1994254a8bb4889e0601144f4dd
Spoondogg <ryan@spoonmedia.ca>
2019-07-27 16:29:50 -0400

    Created PAGINATION class

--- Scripts/src/icarus/model/EL/footer/FOOTER.js
+++ Scripts/src/icarus/model/EL/footer/FOOTER.js
@@ -12,14 +12,10 @@ export default class FOOTER extends EL {
 	/**
 	    Constructs a generic footer.
 	    @param {EL} node The object to contain the footer
-	    @param {MODEL} model The object model
+	    @param {MODEL} [model] The object model
 	 */
 	constructor(node, model) {
 		super(node, 'FOOTER', model);
 	}
 }
-export {
-	ATTRIBUTES,
-	EL,
-	MODEL
-}
+export { ATTRIBUTES, EL, MODEL }
