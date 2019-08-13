89b6e11cfc4f5ca834c4429ee9cb0fa27f860a8b
Spoondogg <ryan@spoonmedia.ca>
2019-08-13 10:42:43 -0400

    Simple error logging for CACHE.js

--- Scripts/src/icarus/model/EL/container/main/CACHE.js
+++ Scripts/src/icarus/model/EL/container/main/CACHE.js
@@ -19,25 +19,34 @@ export default class CACHE extends MODEL {
     */
     cacheObject(type, uid) { //age = 30
         return new Promise((resolve, reject) => {
-            //this[type][uid] = obj;
-            if (parseInt(uid) > 0) {
+            if (this[type][uid] === null) {
+                console.info('CACHE was already attempted', type, uid);
+                resolve(null);
+            } else if (parseInt(uid) > 0) {
                 this.getPayload(uid, type).then((payload) => {
-                    //console.log('Cache.cacheObj', payload);
-                    try {
-                        if (typeof payload.model.jsonResults !== 'undefined') {
-                            payload.model.jsonResults = JSON.parse(payload.model.jsonResults);
+                    if (payload.className === 'ERROR') {
+                        //console.error(this.toString() + '.getPayload', uid, type, payload.message);
+                        this[type][uid] = uid;
+                    } else if (payload.model !== null) {
+                        //console.log('Cache.cacheObj', payload);
+                        try {
+                            if (typeof payload.model.jsonResults !== 'undefined') {
+                                payload.model.jsonResults = JSON.parse(payload.model.jsonResults);
+                            }
+                        } catch (e) {
+                            console.log('Unable to parse json', uid, payload);
                         }
-                    } catch (e) {
-                        console.log('Unable to parse json', uid, payload);
+                        this[type][uid] = payload.model;
                     }
-                    this[type][uid] = payload.model;
                     //console.log('cache', this, age);
                     resolve(this[type][uid]);
                 });
-            //} else if (parseInt(uid) > 0) {
+                //} else if (parseInt(uid) > 0) {
                 //reject(new Error('Failed to cache ' + type + ', ' + uid));
             } else {
-                reject(new Error('Failed to cache ' + type + '.  UId is not valid'));
+                let msg = this.toString() + '.cacheObject: Failed to cache ' + type + '.  UId is not valid';
+                console.error(msg);
+                reject(new Error(msg));
             }
         })
     }
